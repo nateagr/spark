1e808cc4f34c21c399f159c66ec56decda7bed9b
William Montaz <w.montaz@criteo.com>
2019-05-16 15:56:39 +0200

    Lower log level when response cannot be sent to client

--- common/network-common/src/main/java/org/apache/spark/network/server/TransportRequestHandler.java
+++ common/network-common/src/main/java/org/apache/spark/network/server/TransportRequestHandler.java
@@ -289,7 +289,7 @@ public class TransportRequestHandler extends MessageHandler<RequestMessage> {
       if (future.isSuccess()) {
         logger.trace("Sent result {} to client {}", result, remoteAddress);
       } else {
-        logger.error(String.format("Error sending result %s to %s; closing connection",
+        logger.warn(String.format("Error sending result %s to %s; closing connection",
           result, remoteAddress), future.cause());
         channel.close();
       }
